f79e283de99cb0f91ef6fda2c310810369cfd490
msnthr0pe <pavlid66@gmail.com>
2025-05-21 19:58:31 +0300

    Completed navigation from title fragment to login fragment

---
+++ app/src/main/java/com/example/atelierapp/TitleFragment.kt
@@ -0,0 +1,35 @@
+package com.example.atelierapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.atelierapp.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+
+    private var _binding: FragmentTitleBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTitleBinding.inflate(layoutInflater, container, false)
+        binding.btnNext.setOnClickListener {
+            findNavController().navigate(R.id.action_titleFragment_to_loginFragment)
+        }
+        return binding.root
+    }
+
+    companion object {
+
+    }
+}
